f76cb878ee1dd1844365d3d91ab3dbc7198a81b9
gabriellapalma <gabriellar07@icloud.com>
2023-09-21 09:34:09 -0400

    debug

--- Assignment 2/Debug.py
+++ Assignment 2/Debug.py
@@ -5,7 +5,7 @@ def main():
     
     #this is a nice solid working one!
     name = input("your name is? ")
-    print("Hello, ", name)
+    print("Hello,", name)
 
     #this isn't printing anything :( so sad. what's wrong with her? Why she not printing out?
     color = input("what's your favorite color? ")
@@ -16,15 +16,15 @@ def main():
 	
     #Start with this one! We have a compilation error :(
     #Side note: you can put these statements in parentheses or not. it's up to you.
-    if (5 > 3)
+    if 5 > 3:
         print("5 is greater than 3")
     
     #There are multiple correct answers and adjustments to this one 
     isFive = 5
-    if isFive > 5:
-        print("isFive is greater than 5")
+    if isFive > 3:
+        print("5 is greater than 3")
     else:
-        print("isFive is less than 5")
+        print("5 is less than 3")
 
     #more multiple correct answers. Similar to the first. Adjust as perceived 
     toCheck = 4
@@ -33,18 +33,18 @@ def main():
     elif toCheck < 3:
 	    print(toCheck)
     else:
-        print(toCheck, "is 3")
+        print(toCheck, "is 4")
 
     #Is it really printing the BEST option though? Rearrange these as you see fit
     toCheck = 5
     if toCheck < 6:
-        print("toCheck is less than 6")
+        print("5 is less than 6")
     elif toCheck > 7:
-        print("toCheck is greater than 7")
+        print("5 is greater than 7")
     elif toCheck == 5:
-        print("toCheck is 5")
+        print("5 is 5")
     else:
-        print("toCheck is invalid")
+        print("5 is invalid")
     
     
     
