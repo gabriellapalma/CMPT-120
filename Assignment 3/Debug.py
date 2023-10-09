690176d1e69ffeee53d5557c0d19b1928be960dd
gabriellapalma <gabriellar07@icloud.com>
2023-10-09 13:19:53 -0400

    hw

--- Assignment 3/Debug.py
+++ Assignment 3/Debug.py
@@ -1,50 +1,51 @@
-def main():
+
     
-    #Can you print out "Hello" 8 times? I gave you a tiny hint to start...
-    for x in range(?):
+
+word = "Hello"
+for x in range(8):
+    print(word)
         
-    #What about as a while loop?
-    loops = ?
-    while (loops < ?):
+#What about as a while loop?
+word = "Hello"
+i = 0
+while i < 8:
+    print(word)
+    i = i +1
+
         
     
-    #The loop iterations are one behind in a non-programming counting way... how can we fix this?
-    count = 0
-    while (count < 3):   
-        print("While loop iteration", count)
-        count = count + 1
+#The loop iterations are one behind in a non-programming counting way... how can we fix this?
+count = 1
+while (count < 3):
+    print("While loop iteration", count)
+    count = count + 1
         
-    #Same deal here!
-    for x in range(3):
-        print("For loop iteration:", x)
-     
-    #Uh oh I messed up and made an infinite loop... so silly of me!   
-    endless = 4    
-    while (endless < 5):
-        print("I'm stuck in this loop!!!!")
+#Same deal here!
+for x in range(3):
+    print("For loop iteration:", x + 1)
+
+#Uh oh I messed up and made an infinite loop... so silly of me!
+endless = 6
+while (endless < 5):
+    print("I'm stuck in this loop!!!!")
     
-    #print out your last name one letter at a time
-    for x in "":
-        print(?)
+#print out your last name one letter at a time
+list = ("p", "a", "l", "m", "a")
+for x in (list):
+    print(x)
        
-     #aw i suck i made another infinite loop.. use that thing I taught you about to get out once it prints once... starts with a b... br....
-    found == False    
-    while found == False:
-        print("i only printed once")
+#aw i suck i made another infinite loop.. use that thing I taught you about to get out once it prints once... starts with a b... br....
+found = False
+while found == False:
+    print("i only printed once")
+    found = True
         
-    #can you fill this out so that it prints found when it hits the letter r?
-    for x in "Marist":
-        if ? == ?:
-            print("found")
-    
-    numbers = [1,2,3,4,5,6,7,8,9,10]
-    #you could print out the list using print(numbers) OR you could go the long way and use a for loop to print out the value of each index :)
-    for x in ?:
-        print(?[?])
-    
-    #what if I wanted you to print out only the even numbers in this range I made?
-    for x in range (20, 501):
-        #i feeeeel like modulooooooo is neededddd
-        if
-    
-main()
+#can you fill this out so that it prints found when it hits the letter r?
+for x in "Marist":
+    if x == 'r':
+        print("found")
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list)
+
