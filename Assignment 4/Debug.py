d40add9d466c1d33e7e0f59253d6600753f240f4
gabriellapalma <gabriellar07@icloud.com>
2023-11-06 17:33:32 -0500

    hw

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -3,28 +3,53 @@ def printHello():
     print("Hello")
     
 def printName(x):
+    name=("Gabriella")
     print(x)
     
 def addition(x, y):
     #add x and y together and return them
-    return
+    return(x + y)
 
-def smaller(i, j):
-    #if i is smaller than j, return i
-    #if j is smaller than i, return j
-    #if they're even, return 0
+def main():
+    print(add(10,5))
+main()
+i = 10
+j = 5
+def main():
+        def smaller(i, j):
+
+        if (i) < (j):
+            return i
+        if (j) < (i):
+            return j
+        else:
+            return(0)
+
+result = smaller(4,5)
+print(result)
 
 def main():
     
     #call the printHello function here
-    
+    def PrintHello():
+
+
     #call printName and give it the parameter of your name
-    
+    def PrintName():
+        name = ("Gabriella")
+
     var1= 10
     var2= 20
     #What do we put in here to make it work?
-    print(addition())
-    
+    print(addition(var1 + var2))
+
+    if num1 > num2:
+        num1 = smaller
+
+    if num2 > num1:
+        num1 = smaller
+
+
     num1 = int(input("Enter number 1"))
     num2 = int(input("Enter number 2"))
     #what go here?
--- Assignment 4/blackjack.py
+++ Assignment 4/blackjack.py
@@ -2,11 +2,22 @@
 #this script is going to require some googling: I want you to practice using your resources with this one. But of course if you get stuck, reach out :)
 '''instructions: randomly generate three values between 1 and 11. in the function bust: add these three numbers together. if they add up to or less than 21, return the sum. If it's over 21, return 0. If it's over 21 BUT there's an 11 as one of the values, return the sum - 10. '''
 
-def bust():
-    
-    
-    
-def main():
-    
 
-main()
+def add(x, y, z):
+    return x + y + z
+total = add(4, 6, 9)
+
+if total <= 21:
+    print(total)
+
+if total > 21 and total == 21:
+    print(0)
+
+if total >21 and total == 12:
+    print(10)
+
+
+
+
+    
+    
